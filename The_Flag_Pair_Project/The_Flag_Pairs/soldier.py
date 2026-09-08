6bfb37fd78228cbfd5822702a474ad1a21f9c75a
YuvalPJW <yuval100cohen@gmail.com>
2026-09-08 16:41:36 +0300

    Add files via upload

---
+++ The_Flag_Pair_Project/The_Flag_Pairs/soldier.py
@@ -0,0 +1,23 @@
+import pygame
+
+import consts
+import game_field
+import screen
+import main
+
+def hit_mine(solider_location):
+    soldier_left = solider_location[0]+3, solider_location[1]
+    soldier_right = solider_location[0]+3, solider_location[1]+1
+    if soldier_left in game_field.mine_location_list or soldier_right in game_field.mine_location_list:
+        return True
+    return False
+
+def soldier_out_of_field(solider_location):
+    if 0<= solider_location[0] <= consts.BOARD_COLS and 0<= solider_location[1] <= consts.BOARD_ROWS:
+        return True
+    return False
+
+def hit_flag(solider_location):
+    if game_field.field[solider_location[0]][solider_location[1]] == 2 and game_field.field[solider_location[0]][solider_location[1]+1] == 2:
+        return True
+    return False
